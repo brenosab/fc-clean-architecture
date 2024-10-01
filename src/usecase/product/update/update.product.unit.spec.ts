f589597860c598f669b80a7bff1f8a1e87b22a99
Breno <112519@pmenos.com.br>
2024-10-01 20:32:49 -0300

    Adiciona use cases para list e update

---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,31 @@
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import UpdateProductUseCase from "./update.product.usecase";
+const product = ProductFactory.create(
+    "a", "product1", 12.99
+);
+
+const input = {
+    id: product.id,
+    name: "product1",
+    price: 12.99,
+};
+
+const MockRepository = () => {
+  return {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit test for product update use case", () => {
+  it("should update a product", async () => {
+    const productRepository = MockRepository();
+    const productUpdateUseCase = new UpdateProductUseCase(productRepository);
+
+    const output = await productUpdateUseCase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
